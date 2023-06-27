e9b981836c8cc5d42fa1b4899c332c25bd8ce131
Matt Murray <mattm.trinsic@gmail.com>
2023-06-27 13:39:27 -0700

    Update for 7.1.0

--- Table View (Swift)/NativoSwiftTableViewSample/NativoAdView.swift
+++ Table View (Swift)/NativoSwiftTableViewSample/NativoAdView.swift
@@ -12,6 +12,7 @@ class NativoAdView: UIView, NtvAdInterface {
     
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var authorNameLabel: UILabel!
+    @IBOutlet weak var authorImageView: UIImageView!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var previewTextLabel: UILabel!
     @IBOutlet weak var adImageView: UIImageView!
