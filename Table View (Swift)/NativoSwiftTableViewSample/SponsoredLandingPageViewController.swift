40ed19ac98816ed5ebcfa6957952e72af4f46519
Matt Murray <mattm.trinsic@gmail.com>
2024-10-15 14:20:21 -0700

    Fixed issue where landing page would dissapear after CTA link

--- Table View (Swift)/NativoSwiftTableViewSample/SponsoredLandingPageViewController.swift
+++ Table View (Swift)/NativoSwiftTableViewSample/SponsoredLandingPageViewController.swift
@@ -47,7 +47,6 @@ class SponsoredLandingPageViewController: UIViewController, NtvLandingPageInterf
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
-        self.contentWKWebView.stopLoadingNativo()
     }
     
     @objc
